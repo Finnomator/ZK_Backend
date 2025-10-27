82c3dc370bb00ca6a1e1bc47a08327f4811ddcc7
Finn Drünert <finndruenert@gmail.com>
2025-10-27 21:16:17 +0100

    bf

--- app/internal/config.py
+++ app/internal/config.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import quote_plus
 
 from app.internal.helper import hash_password
 
@@ -10,4 +11,6 @@ VEHICLE_PASSWORD = os.getenv("VEHICLE_PASSWORD")
 DEFAULT_ADMIN_NAME = os.getenv("DEFAULT_ADMIN_NAME")
 DEFAULT_ADMIN_HASH = hash_password(os.getenv("DEFAULT_ADMIN_PWD"))
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@db:5432/{DB_NAME}"
+encoded_db_password = quote_plus(DB_PASS)
+
+DATABASE_URL = f"postgresql://{DB_USER}:{encoded_db_password}@db:5432/{DB_NAME}"
