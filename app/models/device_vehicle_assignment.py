948a899721c373af6f9db1ca6ea7f4efdb861252
Finn Drünert <finndruenert@gmail.com>
2026-03-05 21:10:41 +0100

    set end_time optional and ids non null

--- app/models/device_vehicle_assignment.py
+++ app/models/device_vehicle_assignment.py
@@ -4,9 +4,9 @@ from sqlmodel import SQLModel, Field
 
 class _DeviceVehicleAssignmentBase(SQLModel):
     start_time: datetime
-    end_time: datetime
+    end_time: datetime | None = None
 
 class DeviceVehicleAssignmentDB(_DeviceVehicleAssignmentBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    device_imei: str | None = Field(default=None, foreign_key="devicedb.imei")
-    vehicle_id: int | None = Field(default=None, foreign_key="vehicledb.id")
+    device_imei: str = Field(foreign_key="devicedb.imei")
+    vehicle_id: int = Field(foreign_key="vehicledb.id")
