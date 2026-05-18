4afe13bfa6d43b1d82c711595e2baa1fc736e659
Finn Drünert <finndruenert@gmail.com>
2026-05-18 18:09:24 +0200

    import stuff

--- app/models/hw_revision.py
+++ app/models/hw_revision.py
@@ -1,9 +1,7 @@
 from sqlmodel import SQLModel, Field, Relationship
 
-from app.models import DeviceDB
-
 
 class HardwareRevisionDB(SQLModel, table=True):
     revision_number: int = Field(primary_key=True)
-    compatible_firmwares: list["FirmwareDB"] = Relationship(back_populates="compatible_hardware")
-    devices: list[DeviceDB] = Relationship(back_populates="hw_revision")
+    compatible_firmwares: list["FirmwareDB"] | None = Relationship(back_populates="compatible_hardware")
+    devices: list["DeviceDB"] | None = Relationship(back_populates="hw_revision")
