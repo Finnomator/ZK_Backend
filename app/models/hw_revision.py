9f62a25a065306b744595fc536c0fa850eb7443d
Finn Drünert <finndruenert@gmail.com>
2026-05-18 18:44:05 +0200

    remove compatible fws

--- app/models/hw_revision.py
+++ app/models/hw_revision.py
@@ -3,5 +3,4 @@ from sqlmodel import SQLModel, Field, Relationship
 
 class HardwareRevisionDB(SQLModel, table=True):
     revision_number: int = Field(primary_key=True)
-    compatible_firmwares: list["FirmwareDB"] | None = Relationship(back_populates="compatible_hardware")
     devices: list["DeviceDB"] | None = Relationship(back_populates="hw_revision")
